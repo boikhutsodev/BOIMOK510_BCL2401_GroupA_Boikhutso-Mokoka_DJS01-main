86d03eae892f1b7e17a2ab9e939ef16c1d454490
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-05-07 14:26:23 +0200

    completed

--- index.js
+++ index.js
@@ -13,22 +13,16 @@ const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
-
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const d2 = d * time + vel; //calcultes new distance
+const rf = fuel - fbr * time; //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
-}
+calcNewVel = (vel, acc, time) => {
+  return vel + acc * time * 3.6;
+};
+
+const vel2 = calcNewVel(vel, acc, time); //calculates new velocity based on acceleration
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
-
-
-
-
-
-
